d506e650813aff27d1eb428ee3f5ff622cd4cd0b
SuckTinHock <paulpham.kiet@gmail.com>
2022-08-31 22:58:24 -0500

    Page 5

--- pages/firstSong.jsx
+++ pages/firstSong.jsx
@@ -1,5 +1,63 @@
+import firstSongStyles from '../styles/firstSong.module.scss';
+
 const firstSong = () => {
+
     return (<div>
+        <div className={firstSongStyles.title}>DIỄM XƯA - MỐI TÌNH ĐẦU DAY DỨT CỦA TRỊNH CÔNG SƠN</div>
+        <div className={firstSongStyles.gridContainer}>
+            <div style={{fontSize: "24px"}}>Lời bài hát</div>
+            <div></div>
+            <div className={firstSongStyles.item3}>
+                <img src='../images/300786725_816707349687658_5979626644079718832_n.png' />
+            </div>
+            <div>
+                        Mưa vẫn mưa bay trên tầng tháp cổ<br />
+                        Dài tay em mấy thuở mắt xanh xao<br />
+                        Nghe lá thu mưa reo mòn gót nhỏ<br />
+                        Đường dài hun hút cho mắt thêm sâu<br />
+                        <br />
+                        Mưa vẫn hay mưa trên hàng lá nhỏ<br />
+                        Buổi chiều ngồi ngóng những chuyến mưa qua<br />
+                        Trên bước chân em âm thầm lá đổ<br />
+                        Chợt hồn xanh buốt cho mình xót xa<br />
+                        <br />
+                        Chiều nay còn mưa sao em không lại<br />
+                        Nhớ mãi trong cơn đau vùi<br />
+                        Làm sao có nhau, hằn lên nỗi đau<br />
+                        Bước chân em xin về mau<br />
+            </div>
+            <div>
+                    Mưa vẫn hay mưa cho đời biến động<br />
+                    Làm sao em nhớ những vết chim di<br />
+                    Xin hãy cho mưa qua miền đất rộng<br />
+                    Để người phiêu lãng quên mình lãng du<br />
+                    <br />
+                    Chiều nay còn mưa sao em không lại<br />
+                    Nhớ mãi trong cơn đau vùi<br />
+                    Làm sao có nhau, hằn lên nỗi đau<br />
+                    Bước chân em xin về mau<br />
+                    <br />
+                    Mưa vẫn hay mưa cho đời biến động<br />
+                    Làm sao em biết bia đá không đau<br />
+                    Xin hãy cho mưa qua miền đất rộng<br />
+                    Ngày sau sỏi đá cũng cần có nhau<br />
+            </div>
+            
+        </div>
+        <div className={firstSongStyles.gridContainer}>
+            <div className={firstSongStyles.text}>
+                Ngô Vũ Bích Diễm - Thời Sinh Viên 
+            </div>
+            <div><img src='../images/299217296_453356333507652_3903324527003938303_n.png' style={{width: "200px"}}/></div>
+            <div className={firstSongStyles.item7}>
+                <div style={{fontSize: "20px", marginBottom: "20px"}}>Ý nghĩa ca khúc</div>
+                <div>Được sáng tác vào năm 1960, "Diễm xưa" được coi là một trong những ca khúc đầu tay trong sự nghiệp nhạc tình của nhạc sĩ Trịnh Công Sơn. Bài hát chất chứa một nỗi niềm rất thật của người nghệ sĩ, vừa tiếc nuối vừa trách móc, ưu thương. Với những người mộ điệu, họ cảm nhận được trong "Diễm xưa" hình ảnh một chàng trai đang mòn mỏi ngóng chờ những bước chân âm thầm của một cô gái "chiều nay còn mưa sao em không lại”. Trách móc đấy nhưng vẫn trông ngóng mãi một bóng hình. </div>
+            </div>
+        </div>
+    </div>);
+
+    return (<div>
+        <div className={firstSongStyles.title}>DIỄM XƯA - MỐI TÌNH ĐẦU DAY DỨT CỦA TRỊNH CÔNG SƠN</div>
         <table>
             <tbody>
                 <tr>
@@ -58,7 +116,7 @@ const firstSong = () => {
                 </tr>
             </tbody>
         </table>
-    </div>)
+    </div>);
 }
 
 export default firstSong;
